1c5cd06dfdf8b7686215eaf6209b901671f51a4d
Steffen Cruz <steffenjcruz@gmail.com>
2023-11-06 21:19:14 -0600

    Move protocol to folding dir

---
+++ folding/protocol.py
@@ -0,0 +1,51 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+# TODO(developer): Set your name
+# Copyright © 2023 <your name>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import typing
+import bittensor as bt
+
+import folding
+
+
+class Synapse(bt.Synapse):
+    """
+    A protocol representation which uses bt.Synapse as its base.
+    This protocol helps in handling request and response communication between
+    the miner and the validator.
+
+    Attributes:
+    - protein: A Protein object, which contains the necessary details of the protein to be folded.
+    """
+
+    # Required request input, filled by sending dendrite caller.
+    protein: folding.protein.Protein
+
+    # Optional request output, filled by recieving axon.
+    md_output: typing.Optional[dict] = None
+
+    def deserialize(self) -> int:
+        """
+        Deserialize the output. This method retrieves the response from
+        the miner in the form of a bytestream, deserializes it and returns it
+        as the output of the dendrite.query() call.
+
+        Returns:
+        - dict: The serialized response, which in this case is the value of md_output.
+        """
+        return self.md_output
